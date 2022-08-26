05f0aa20d1c0768bd87e84e4adda2ab1a6f4d645
Dev <devkumarpatel20@gnu.ac.in>
2022-08-26 17:16:25 +0530

    practical 2

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.practical2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun showMessage(msg:String){
+        Log.i(TAG,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.snackbarId),msg,Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        showMessage("onResume function called.")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        showMessage("onRestart function called.")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        showMessage("onPause function called.")
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        showMessage("onDestroy function called.")
+        super.onDestroy()
+    }
+
+    override fun onStop() {
+        showMessage("onStop function called.")
+        super.onStop()
     }
 }
