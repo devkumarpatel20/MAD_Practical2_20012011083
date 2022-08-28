827afbc1cb26ad307098a1bbe2e2799100b8e4b2
Dev <devkumarpatel20@gnu.ac.in>
2022-08-28 20:43:05 +0530

    practical 2

--- app/src/main/java/com/example/practical2/MainActivity.kt
+++ app/src/main/java/com/example/practical2/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        showMessage("onCreate function called.")
+        showMessage("onCreate function called.")
     }
 
     fun showMessage(msg:String){
